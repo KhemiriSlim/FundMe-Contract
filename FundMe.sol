40e6e90478c82d5a70424c31cbee8941140bdeda
KhemiriSlim <khemirislim04@gmail.com>
2026-07-06 17:15:18 +0100

    complete the withdraw

--- FundMe.sol
+++ FundMe.sol
@@ -9,15 +9,29 @@ contract FundMe{
     address[] public funders;
     mapping (address funder=> uint256 amountFunded) public addressToAmountFunded;
 
+    address public owner;
+    constructor(){
+        owner = msg.sender;
+    }
+
+
     function fund() public payable{
         require(msg.value.getConvertionRate() >= minimumUsd, "didn t send enough ETH");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
     }
-    function withdraw() public {
+    function withdraw() public onlyOwner{
         for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
             address funder =  funders[funderIndex];
             addressToAmountFunded[funder] = 0;
         }
+        funders = new address[](0);
+        //payable (msg.sender).transfer(address(this).balance);
+        (bool callsuccess, ) = payable (msg.sender).call{value: address(this).balance}("");
+        require(callsuccess,"Call failed");
+    }
+    modifier onlyOwner(){
+        require(msg.sender == owner,"Sender is not owner!");
+        _;//complete excution the function
     }
 }
