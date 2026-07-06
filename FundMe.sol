dbdbbb99bb022874038ef0b6d00dd569ed8d7dc3
KhemiriSlim <khemirislim04@gmail.com>
2026-07-06 14:27:13 +0100

    add withdraw function

--- FundMe.sol
+++ FundMe.sol
@@ -4,9 +4,7 @@ pragma solidity ^0.8.18;
 import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe{
-
     using PriceConverter for uint256;
-
     uint public minimumUsd = 5e18;
     address[] public funders;
     mapping (address funder=> uint256 amountFunded) public addressToAmountFunded;
@@ -16,5 +14,10 @@ contract FundMe{
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
     }
-    
+    function withdraw() public {
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+            address funder =  funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+    }
 }
